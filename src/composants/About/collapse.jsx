abe28869aca20874af8b4b18a43d97b61770b7cc
safa dorii <safadorai23@gmail.com>
2024-04-04 10:16:48 +0200

    modification du code

--- src/composants/About/collapse.jsx
+++ src/composants/About/collapse.jsx
@@ -9,13 +9,13 @@ const angleUp = <FontAwesomeIcon icon={faAngleUp} />
 const Collapse = (props) => {
     const [ openTab, setOpenTab ] = useState(false);
 
-    const openTabHandler = () => {
+    const opencollapse = () => {
         setOpenTab((openTab) => !openTab); 
     };
 
     return (
         <>
-            <div onClick={openTabHandler} className="collapse_header">
+            <div onClick={opencollapse} className="collapse_header">
                 <h2 className="collapse_header_title">{props.title}</h2>
                 {!openTab ? <i className="collapse_header_icon">{angleDown}</i> : <i className="collapse_header_icon">{angleUp}</i>}
             </div>
